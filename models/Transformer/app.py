611a796036b755cc57963f3319db20dcd32aadc5
Dennis Zyska <dennis@zyska.org>
2022-11-16 13:57:32 +0100

    added simple transformer pipeline for text classification

--- models/Transformer/app.py
+++ models/Transformer/app.py
@@ -15,15 +15,14 @@ import time
 import logging
 import torch
 import yaml
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
+from transformers import pipeline
 
 if __name__ == '__main__':
     print("Load environment variables...")
     load_dotenv()
 
     print("Setting up logger to level {}...".format(os.getenv("LOG_LEVEL")))
-    logging.basicConfig( level=os.getenv("LOG_LEVEL").upper() )
+    logging.basicConfig(level=os.getenv("LOG_LEVEL").upper())
 
     print("Load skill config...")
     with open(os.getenv("SKILL_FILE"), "r") as f:
@@ -32,31 +31,26 @@ if __name__ == '__main__':
     logging.info("Create SocketIO Client...")
     sio = socketio.Client()
 
-    logging.info("Loading AutoTokenizer...")
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=skill['model_name'])
+    logging.info("Transformers pipeline...")
+    pipe = pipeline(**skill['pipeline'])
 
-    logging.info("Loading model...")
-    model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name_or_path=skill['model_name'])
+    @sio.on("task")
+    def task(data):
+        logging.info("Received new task: {}".format(data))
+        sio.emit('results', {'task_id': data['task_id'], 'results': pipe(data['data'])})
 
-    '''
-    # TODO load model and checkpoint
-    # see https://pytorch-lightning.readthedocs.io/en/stable/deploy/production_intermediate.html
-    model = MyModel()
-    checkpoint = torch.load(os.getenv("MODEL_PATH"))
-    model.load_state_dict(checkpoint["state_dict"])
-    model.eval()
-    '''
 
     @sio.on('connect')
     def connect():
         logging.info('Connection established!')
         sio.emit('register_skill', skill)
 
+
     logging.info("Connect to broker...")
-    while(True):
+    while True:
         try:
             logging.info("Connect to broker {}".format(os.getenv("BROKER_URL")))
-            sio.connect(os.getenv('BROKER_URL'), auth= { "token": os.getenv('BROKER_TOKEN')} )
+            sio.connect(os.getenv('BROKER_URL'), auth={"token": os.getenv('BROKER_TOKEN')})
             sio.wait()
         except:
             logging.error("Connection to broker failed. Trying again in 5 seconds ...")
