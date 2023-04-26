9ab64b184cf151bad6cac745cb9994adf06f966b
Dennis Zyska <dennis@zyska.org>
2023-04-26 12:21:08 +0200

    fasten up checking db pending status

--- broker/db/__init__.py
+++ broker/db/__init__.py
@@ -5,6 +5,6 @@ def results(job):
     """Return the job result of an arango db async operation.
     """
     while job.status() == "pending":
-        time.sleep(0.1)
+        time.sleep(0.01)
 
     return job.result()
