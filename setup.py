f372a5733491f0f7616639e947cc9a1e8687356f
Dennis Zyska <dennis@zyska.org>
2023-10-10 13:21:30 +0200

    separate requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name="nlp_broker",
+    name="nlp-broker",
     version='0.3.0',
     author="Ubiquitous Knowledge Processing (UKP) Lab",
     author_email="dennis.zyska@tu-darmstadt.de",
